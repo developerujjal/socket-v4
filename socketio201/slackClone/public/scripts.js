102eeb5ea7466a4782cc76d35c34129e4839a2bb
developerujjal <ujjal.vision02@gmail.com>
2025-05-06 22:03:22 +0600

    display the namespaces dynamically

---
+++ socketio201/slackClone/public/scripts.js
@@ -0,0 +1,27 @@
+
+// const userName = prompt('What is your username?');
+// const password = prompt('What is your password');
+
+const socket = io();
+
+socket.on('connect', () => {
+    console.log(socket.id)
+});
+
+// nsList event for get the namespace from server
+socket.on('nsList', (nsData) => {
+    console.log(nsData)
+
+    const namespacesDiv = document.querySelector('.namespaces');
+    nsData.forEach((ns) => {
+        namespacesDiv.innerHTML += `
+                <div class="namespace" ns=${ns.name}>
+                    <img src=${ns.image}>
+                </div>
+        `;
+
+
+    });
+});
+
+
---
+++ socketio201/slackClone/slack.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const app = express();
+const { createServer } = require('http');
+const { Server } = require('socket.io');
+const namespaces = require('./data/namespaces');
+
+app.use(express.static(__dirname + '/public'));
+
+const httpServer = createServer(app);
+
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*',
+        methods: ['GET', 'POST'],
+        allowedHeaders: ['Content-Type'],
+        credentials: true,
+    },
+});
+
+
+io.on('connection', (socket) => {
+    console.log(socket.id + ' has connected');
+
+    socket.emit('nsList', namespaces);
+
+
+})
+
+
+
+
+httpServer.listen(3000, () => {
+    console.log('Server is running on port 3000');
+})
