d63567e8045dfae79147e5ff24d611e6d869d412
developerujjal <ujjal.vision02@gmail.com>
2025-05-06 23:24:41 +0600

    display all rooms dynamically

--- socketio201/slackClone/public/scripts.js
+++ socketio201/slackClone/public/scripts.js
@@ -19,13 +19,35 @@ socket.on('nsList', (nsData) => {
     const namespacesDiv = document.querySelector('.namespaces');
     nsData.forEach((ns) => {
         namespacesDiv.innerHTML += `
-                <div class="namespace" ns=${ns.name}>
+                <div class="namespace" ns=${ns.endpoint}>
                     <img src=${ns.image}>
                 </div>
         `;
-
-
     });
+
+
+    Array.from(document.getElementsByClassName('namespace')).forEach((element) => {
+        console.log(element)
+        element.addEventListener('click', (e) => {
+            const nsEndPoint = element.getAttribute('ns');
+            console.log(nsEndPoint)
+
+            const clickedNs = nsData.find(ns => ns.endpoint === nsEndPoint);
+            console.log(clickedNs)
+
+            const roomListDiv = document.querySelector('.room-list');
+
+            roomListDiv.innerHTML = '';
+            
+            clickedNs.rooms.forEach((room) => {
+                roomListDiv.innerHTML += `
+                        <li><span class="glyphicon glyphicon-globe"></span>${room.roomTitle}</li>
+                `
+            } )
+        })
+    });
+
+
 });
 
 
